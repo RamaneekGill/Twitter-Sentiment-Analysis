521950057f008d400e5d092c0955f3387664d9bf
Ramaneek Gill <ramaneek.gill@hotmail.com>
2016-03-26 16:05:46 -0400

    Creates functionality for removing neutral tweets from dataset

--- parse_dataset.py
+++ parse_dataset.py
@@ -33,10 +33,32 @@ def parse_corpus(corpus):
 		inputs:  A numpy array of the tweets
 		targets: A numpy array of the sentiment, 1 for positive, 0 for negative
 	"""
+	print('parsing corpus')
 	corpus.columns = ["sentiment", "2", "3", "4", "5", "tweet"]
 	inputs = np.array(corpus["tweet"])
 	targets = np.array(corpus["sentiment"])
+	print('parsed corpus into numpy arrays')
 
+	return inputs, targets
+
+def remove_neutral_tweets(inputs, targets):
+	"""
+	Parses the corpus and returns the inputs and targets
+
+	Returns:
+		inputs:  A numpy array of the tweets
+		targets: A numpy array of the sentiment, 1 for positive, 0 for negative
+	"""
+	print('removing neutral tweets')
+
+	count = 0
+	for i in range(len(inputs)):
+		if targets[i] == 2: # Remove tweets with neutral sentiment
+			count += 1
+			np.delete(inputs, i)
+			np.delete(targets, i)
+
+	print('Removed {0} neutral tweets'.format(count))
 	return inputs, targets
 
 def main():
@@ -47,6 +69,10 @@ def main():
 
 	corpus = load_csv()
 	inputs, targets = parse_corpus(corpus)
+
+	if not 'keep_neutral_tweets' in cli_args:
+		inputs, targets = remove_neutral_tweets(inputs, targets)
+
 	exit()
 
 
